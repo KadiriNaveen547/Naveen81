c5cc7af36c0edf5e24945ab57060b76f056fd3e4
KadiriNaveen547 <149255399+KadiriNaveen547@users.noreply.github.com>
2025-06-26 10:20:11 +0530

    First Commit
    I  have done task

---
+++ patterns/example.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Example Pattern
+ * Slug: my-custom-block-theme/example
+ * Categories: text
+ */
+?>
+<!-- wp:paragraph -->
+<p>This is an example pattern.</p>
+<!-- /wp:paragraph -->
